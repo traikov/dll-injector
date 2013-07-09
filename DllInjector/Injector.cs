3bd734d6beb89b1deb9fd6cac7d1d93057f54c3c
traikov <traikovdev@gmail.com>
2013-07-09 23:56:32 +0300

    Removed unnecessary using directives.

--- DllInjector/Injector.cs
+++ DllInjector/Injector.cs
@@ -15,8 +15,6 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Diagnostics;
 using DllInjector.Win32;
